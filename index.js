8b7c256e935eb909e5b531fef145fbfae77a8e5a
Kamranmusinov <musinovkamran817@gmail.com>
2024-06-06 17:57:14 +0500

    github setup

---
+++ index.js
@@ -0,0 +1,21 @@
+// let name = prompt('Введите ваше имя:');
+// if (name === 'Alex') {
+//     let accountNumber = prompt('Введите номер вашего счета:');
+//     if (accountNumber === '7777') {
+//         let amount = parseInt(prompt('Введите сумму для обналичивания:'));
+//         if (amount <= 10000) {
+//             alert('Все отлично.');
+//             let withdrawn = amount;
+//             let remainingBalance = 10000 - withdrawn;
+//             alert(`Вы сняли ${withdrawn} рублей. Остаток на вашем счету: ${remainingBalance} рублей.`);
+//         } else {
+//             alert('Недостаточно средств.');
+//         }
+//     } else {
+//         alert('Пользователь не найден.');
+//     }
+// } else {
+//     alert('Пользователь не найден.');
+// }
+
+
