c62e6704f4721ee64e7b5a5a018bd044301bd4d9
Kamranmusinov <musinovkamran817@gmail.com>
2024-06-11 16:40:14 +0500

    kamran

--- index.js
+++ index.js
@@ -19,3 +19,11 @@
 // }
 
 
+let arr = [[{ a: { price: 20 } }], [{ a: { price: 35 } }], [{ a: { price: 44 } }]];
+let total = 0;
+
+for (let i = 0; i < arr.length; i++) {
+  total += arr[i][0].a.price;
+}
+
+console.log(total);
